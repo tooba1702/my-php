4bac90af78220a3db7f6db07379991a4ca3d589d
Tooba Nawaz <nawaztooba11@gmail.com>
2024-06-25 20:50:58 -0700

    save my php file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$name = "tooba";
+$email  = "tooba@gmail.com";
+$age = 20;
+$password =  17024;
+$city  = "karachi";
+
+
+echo "<h1 style='color:red'>$name</h1>";
+echo "<p>my name is $name</p>";
+echo "<h1 style='color:yellow'>$email</h1>";
+echo "<p>my email address is $email</p>";
+echo "<script>console.log('$password')</script>";
+echo "<script>document.write('$age')</script>";
+
+
+echo "<p style='color:pink'>my name is $name</p>"."<p style='color:pink'>my email address is $email</p>"."<p style='color:pink'>I am  $age years old</p>".$password."<p style='color:pink'>I  Live in   $city</p>";
+
+
+
+?>
